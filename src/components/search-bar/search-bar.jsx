c9c1c66f406abbf754ca9633a3ee857fc5840428
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2025-06-09 20:36:15 +0200

    SearchBar style added

--- src/components/search-bar/search-bar.jsx
+++ src/components/search-bar/search-bar.jsx
@@ -1,13 +1,22 @@
-import { Paper } from '@mui/material'
+import { Search } from '@mui/icons-material'
+import { IconButton, Paper } from '@mui/material'
 import { colors } from '../../constants/colors'
 
 const SearchBar = () => {
 	return (
 		<Paper
 			component={'form'}
-			sx={{ border: `1px solid ${colors.secondary}`, pl: 2, boxShadow: 'none' }}
+			sx={{
+				border: `1px solid ${colors.secondary}`,
+				pl: 2,
+				boxShadow: 'none',
+				borderRadius: 'none',
+			}}
 		>
-			<input placeholder='Search...' type='text' />
+			<input placeholder='Search...' type='text' className='search-bar' />
+			<IconButton>
+				<Search />
+			</IconButton>
 		</Paper>
 	)
 }
