6be3f4cc22ba34d15ce1271a19f5a6fb54f5ea81
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2025-06-11 23:59:27 +0200

    Category Types

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,26 @@
-import { Home, OndemandVideo } from '@mui/icons-material'
+import {
+	Checkroom,
+	DeveloperMode,
+	FitnessCenter,
+	GraphicEq,
+	Home,
+	LiveTv,
+	OndemandVideo,
+	School,
+	SportsEsports,
+	TheaterComedy,
+} from '@mui/icons-material'
 
 export const logo = 'https://logowik.com/content/uploads/images/684_metube.jpg'
 export const category = [
 	{ name: 'New', icon: <Home /> },
 	{ name: 'Movie', icon: <OndemandVideo /> },
+	{ name: 'Live', icon: <LiveTv /> },
+	{ name: 'Gaming', icon: <SportsEsports /> },
+	{ name: 'Education', icon: <School /> },
+	{ name: 'Sport', icon: <FitnessCenter /> },
+	{ name: 'Comedy', icon: <TheaterComedy /> },
+	{ name: 'Podcast', icon: <GraphicEq /> },
+	{ name: 'Fashion', icon: <Checkroom /> },
+	{ name: 'Crypto', icon: <DeveloperMode /> },
 ]
