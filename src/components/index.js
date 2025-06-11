720e2c2eb72eb8628019ca9cddafec8a5d1ffec0
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2025-06-11 23:51:07 +0200

    Category Component Add

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export { default as App } from './app/app'
+export { default as Category } from './category/category'
 export { default as Channel } from './channel/channel'
 export { default as Main } from './main/main'
 export { default as Navbar } from './navbar/navbar'
