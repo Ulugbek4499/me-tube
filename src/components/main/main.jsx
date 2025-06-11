b9f78fa88af6e52c96167eee784db7fba61050bb
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2025-06-11 23:51:29 +0200

    Category Componenet Add

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,11 +1,24 @@
-import { Button } from '@mui/material'
-import { Link } from 'react-router-dom'
+import { Box, Container, Stack, Typography } from '@mui/material'
+import { useState } from 'react'
+import { Category } from '../'
+import { colors } from '../../constants/colors'
 
 const Main = () => {
+	const [selectedCategory, setCategory] = useState('New')
+
 	return (
-		<Link to={'/channel'}>
-			<Button>Channel</Button>
-		</Link>
+		<Stack>
+			<Category />
+			<Box p={2} sx={{ height: '90vh' }}>
+				<Container maxWidth='90%'>
+					<Typography variant={'h4'} fontWeight={'bold'} mb={2}>
+						{selectedCategory}
+						<span style={{ color: colors.Secondary }}>Videos</span>
+					</Typography>
+					Videos
+				</Container>
+			</Box>
+		</Stack>
 	)
 }
 
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,1 +1,7 @@
+import { Home, OndemandVideo } from '@mui/icons-material'
+
 export const logo = 'https://logowik.com/content/uploads/images/684_metube.jpg'
+export const category = [
+	{ name: 'New', icon: <Home /> },
+	{ name: 'Movie', icon: <OndemandVideo /> },
+]
