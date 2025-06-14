af23433170b449f6ee8a50223d047bd5ce2ee7a9
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2025-06-14 16:21:17 +0200

    Axios library added

--- src/service/api.service.js
+++ src/service/api.service.js
@@ -1,14 +1,14 @@
+const BASE_URI = 'https://youtube-v31.p.rapidapi.com/'
+
 const options = {
 	params: {
 		maxResults: '50',
 	},
 	headers: {
 		'x-rapidapi-key': process.env.REACT_APP_PUBLIC_API_KEY,
-		'x-rapidapi-host': 'youtube-v31.p.rapidapi.com',
+		'x-rapidapi-host': BASE_URI,
 	},
 }
-
-const BASE_URI = 'https://youtube-v31.p.rapidapi.com/search'
 
 export const ApiService = {
 	async fetching() {},
