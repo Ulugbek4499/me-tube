2608a83391955eb3a2b57589337aa2b6b7138abf
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2025-06-16 12:54:08 +0200

    Channel Card Component added

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 export { default as App } from './app/app'
 export { default as Category } from './category/category'
+export { default as ChannelCard } from './channel-card/channel-card'
 export { default as Channel } from './channel/channel'
 export { default as Main } from './main/main'
 export { default as Navbar } from './navbar/navbar'
