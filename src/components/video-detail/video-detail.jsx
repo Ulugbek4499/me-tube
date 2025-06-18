9d826233ecf922b6c67930632f17d17df4a7a266
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2025-06-18 13:01:51 +0200

    Style for video details added

--- src/components/video-detail/video-detail.jsx
+++ src/components/video-detail/video-detail.jsx
@@ -1,5 +1,7 @@
-import { Box } from '@mui/material'
+import { Tag } from '@mui/icons-material'
+import { Box, Chip, Typography } from '@mui/material'
 import { useEffect, useState } from 'react'
+import ReactPlayer from 'react-player'
 import { useParams } from 'react-router-dom'
 import { ApiService } from '../../service/api.service'
 
@@ -20,15 +22,37 @@ const VideoDetail = () => {
 		}
 		getData()
 	}, [id])
-	const {
-		snippet: { title, channelId, channelTitle, description, tags, thumnails },
-		statistics: { viewCount, likeCount, commentCount },
-	} = videoDetail
+	// const {
+	// 	snippet: { title, channelId, channelTitle, description, tags, thumnails },
+	// 	statistics: { viewCount, likeCount, commentCount },
+	// } = videoDetail
 
 	return (
 		<Box minHeight={'90vh'} mb={10}>
 			<Box display={'flex'}>
-				<Box width={'75%'}></Box>
+				<Box width={'75%'}>
+					<ReactPlayer
+						url={`https://www.youtube.com/watch?v=${id}`}
+						className='react-player'
+						controls
+					/>
+					{videoDetail.snippet.tags.map((item, idx) => (
+						<Chip
+							label={item}
+							key={idx}
+							sx={{ marginTop: '10px', cursor: 'pointer', ml: '10px' }}
+							deleteIcon={<Tag />}
+							onDelete={() => {}}
+							variant='outlined'
+						/>
+					))}
+					<Typography variant='h5' fontWeight={'bold'} p={2}>
+						{videoDetail?.snippet?.title}
+					</Typography>
+					<Typography variant='subtitle2' p={2} sx={{ opacity: '.7' }}>
+						{videoDetail?.snippet?.title}
+					</Typography>
+				</Box>
 				<Box width={'25%'}>Suggested Video</Box>
 			</Box>
 		</Box>
