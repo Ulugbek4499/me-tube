925a768d319930f89bb5ae5f9693c6c9c2f45604
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2025-06-18 12:39:40 +0200

    video deatil conatiner added

--- src/components/video-card/video-card.jsx
+++ src/components/video-card/video-card.jsx
@@ -8,6 +8,7 @@ import {
 	Typography,
 } from '@mui/material'
 import moment from 'moment'
+import { Link } from 'react-router-dom'
 import { colors } from '../../constants/colors'
 
 const VideoCard = ({ video }) => {
@@ -19,14 +20,16 @@ const VideoCard = ({ video }) => {
 				borderRadius: 0,
 			}}
 		>
-			<CardMedia
-				image={video?.snippet?.thumbnails?.high?.url}
-				alt={video?.snippet?.title}
-				sx={{
-					width: { xs: '100%', sm: '360px' },
-					height: '180px',
-				}}
-			/>
+			<Link to={`/video/${video.id.videoId}`}>
+				<CardMedia
+					image={video?.snippet?.thumbnails?.high?.url}
+					alt={video?.snippet?.title}
+					sx={{
+						width: { xs: '100%', sm: '360px' },
+						height: '180px',
+					}}
+				/>
+			</Link>
 			<CardContent
 				sx={{
 					background: colors.primary,
@@ -34,7 +37,7 @@ const VideoCard = ({ video }) => {
 					position: 'relative',
 				}}
 			>
-				<>
+				<Link to={`/video/${video.id.videoId}`}>
 					<Typography my={'5px'} sx={{ opacity: '.4' }}>
 						{moment(video?.snippet?.publishedAt).fromNow()}
 					</Typography>
@@ -44,7 +47,7 @@ const VideoCard = ({ video }) => {
 					<Typography variant='subtitle2' sx={{ opacity: '.6' }}>
 						{video?.snippet?.description.slice(0, 70)}
 					</Typography>
-				</>
+				</Link>
 				<>
 					<Stack
 						direction={'row'}
--- src/components/video-detail/video-detail.jsx
+++ src/components/video-detail/video-detail.jsx
@@ -1,5 +1,38 @@
+import { Box } from '@mui/material'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { ApiService } from '../../service/api.service'
+
 const VideoDetail = () => {
-	return <div>VideoDetail</div>
+	const [videoDetail, setVideoDetail] = useState(null)
+	const { id } = useParams()
+	useEffect(() => {
+		const getData = async () => {
+			try {
+				const data = await ApiService.fetching(
+					`videos?part=snippet,statistics&id=${id}`
+				)
+
+				setVideoDetail(data.items[0])
+			} catch (error) {
+				console.log(error)
+			}
+		}
+		getData()
+	}, [id])
+	const {
+		snippet: { title, channelId, channelTitle, description, tags, thumnails },
+		statistics: { viewCount, likeCount, commentCount },
+	} = videoDetail
+
+	return (
+		<Box minHeight={'90vh'} mb={10}>
+			<Box display={'flex'}>
+				<Box width={'75%'}></Box>
+				<Box width={'25%'}>Suggested Video</Box>
+			</Box>
+		</Box>
+	)
 }
 
 export default VideoDetail
