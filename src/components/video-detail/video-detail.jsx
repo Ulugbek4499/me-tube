9cb187d44a747b1f588c758c79b53c8519b3e04b
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2025-06-21 12:03:44 +0200

    Suggested Video added to Video Details page

--- src/components/video-detail/video-detail.jsx
+++ src/components/video-detail/video-detail.jsx
@@ -4,16 +4,18 @@ import {
 	Tag,
 	Visibility,
 } from '@mui/icons-material'
-import { Box, Chip, Stack, Typography } from '@mui/material'
+import CheckCircleIcon from '@mui/icons-material/CheckCircle'
+import { Avatar, Box, Chip, Stack, Typography } from '@mui/material'
 import { useEffect, useState } from 'react'
 import ReactPlayer from 'react-player'
 import renderHTML from 'react-render-html'
 import { useParams } from 'react-router-dom'
-import { Loader } from '../'
+import { Loader, Videos } from '../'
 import { ApiService } from '../../service/api.service'
 
 const VideoDetail = () => {
 	const [videoDetail, setVideoDetail] = useState(null)
+	const [relatedVideo, setRelatedVideo] = useState([])
 	const { id } = useParams()
 	useEffect(() => {
 		const getData = async () => {
@@ -21,8 +23,12 @@ const VideoDetail = () => {
 				const data = await ApiService.fetching(
 					`videos?part=snippet,statistics&id=${id}`
 				)
-
 				setVideoDetail(data.items[0])
+
+				const relatedData = await ApiService.fetching(
+					`search?part=snippet&relatedToVideoId=${id}&type=video`
+				)
+				setRelatedVideo(relatedData.items)
 			} catch (error) {
 				console.log(error)
 			}
@@ -38,8 +44,8 @@ const VideoDetail = () => {
 
 	return (
 		<Box minHeight={'90vh'} mb={10}>
-			<Box display={'flex'}>
-				<Box width={'75%'}>
+			<Box display={'flex'} sx={{ flexDirection: { xs: 'column', md: 'row' } }}>
+				<Box width={{ xs: '100%', md: '75%' }}>
 					<ReactPlayer
 						url={`https://www.youtube.com/watch?v=${id}`}
 						className='react-player'
@@ -56,39 +62,77 @@ const VideoDetail = () => {
 						/>
 					))}
 					<Typography variant='h5' fontWeight={'bold'} p={2}>
-						{videoDetail?.snippet?.title}
+						{videoDetail.snippet.title}
 					</Typography>
 					<Typography variant='subtitle2' p={2} sx={{ opacity: '.7' }}>
-						{renderHTML(videoDetail?.snippet?.description)}
+						{renderHTML(videoDetail.snippet.description)}
 					</Typography>
 					<Stack direction='row' gap='20px' alignItems='center' py={1} px={2}>
-						<Visibility />
-						{parseInt(videoDetail.statistics.viewCount).toLocaleString()} views
-					</Stack>
-					<Stack
-						sx={{ opacity: 0.7 }}
-						direction='row'
-						alignItems='center'
-						gap='3px'
-					>
-						<FavoriteOutlined />
-						{parseInt(videoDetail.statistics.likeCount).toLocaleString()} likes
-					</Stack>
+						<Stack
+							sx={{ opacity: 0.7 }}
+							direction='row'
+							alignItems='center'
+							gap='3px'
+						>
+							<Visibility />
+							{parseInt(videoDetail.statistics.viewCount).toLocaleString()}{' '}
+							views
+						</Stack>
+						<Stack
+							sx={{ opacity: 0.7 }}
+							direction='row'
+							alignItems='center'
+							gap='3px'
+						>
+							<FavoriteOutlined />
+							{parseInt(videoDetail.statistics.likeCount).toLocaleString()}{' '}
+							likes
+						</Stack>
 
-					<Stack
-						sx={{ opacity: 0.7 }}
-						direction='row'
-						alignItems='center'
-						gap='3px'
-					>
-						<MarkChatRead />
-						{parseInt(
-							videoDetail.statistics.commentCount
-						).toLocaleString()}{' '}
-						comment
+						<Stack
+							sx={{ opacity: 0.7 }}
+							direction='row'
+							alignItems='center'
+							gap='3px'
+						>
+							<MarkChatRead />
+							{parseInt(
+								videoDetail.statistics.commentCount
+							).toLocaleString()}{' '}
+							comment
+						</Stack>
+					</Stack>
+					<Stack direction={'row'} py={1} px={2}>
+						<Stack
+							direction={'row'}
+							alignItems={'center'}
+							gap={'5px'}
+							marginTop={'5px'}
+						>
+							<Avatar
+								alt={videoDetail.snippet.channelTitle}
+								src={videoDetail.snippet.thumbnails.default.url}
+							/>
+							<Typography variant='subtitle2' color='gray'>
+								{videoDetail.snippet.channelTitle}
+								<CheckCircleIcon
+									sx={{ fontSize: '12px', color: 'gray', ml: '5px' }}
+								/>
+							</Typography>
+						</Stack>
 					</Stack>
 				</Box>
-				<Box width={'25%'}>Suggested Video</Box>
+				<Box
+					width={{ xs: '100%', md: '25%' }}
+					px={2}
+					py={{ md: 1, xs: 5 }}
+					justifyContent='center'
+					alignItems='center'
+					overflow={'scroll'}
+					maxHeight={'120vh'}
+				>
+					<Videos videos={relatedVideo} />
+				</Box>
 			</Box>
 		</Box>
 	)
